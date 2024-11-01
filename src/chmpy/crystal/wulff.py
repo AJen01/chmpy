9b06820bee1023b423a3645c212a1b01ecf877d3
AJen01 <71494089+AJen01@users.noreply.github.com>
2024-11-01 08:52:05 +0800

    safe division in wulff construction

--- src/chmpy/crystal/wulff.py
+++ src/chmpy/crystal/wulff.py
@@ -1,6 +1,14 @@
-from scipy.spatial import ConvexHull
+import logging
+from scipy.spatial import ConvexHull  # pylint: disable=no-name-in-module
 import numpy as np
-from chmpy.crystal import Crystal
+
+LOG = logging.getLogger(__name__)
+
+
+def safe_divide(a, b):
+    tiny = np.finfo(float).eps  # Use machine epsilon to avoid zero division
+    return np.divide(a, np.where(b == 0, tiny, b))
+
 
 def expand_symmetry_related_planes(hkl, energies, spacegroup):
     nfacets = hkl.shape[0]
@@ -9,8 +17,9 @@ def expand_symmetry_related_planes(hkl, energies, spacegroup):
     tiled_energies = np.tile(energies, nsymop)
 
     for i, s in enumerate(spacegroup.symmetry_operations):
-        expanded_facets[i * nfacets: (i + 1) * nfacets] =\
-                (hkl @ s.rotation.T).astype(int)
+        expanded_facets[i * nfacets : (i + 1) * nfacets] = (hkl @ s.rotation.T).astype(
+            int
+        )
 
     # identify the unique directions
     # we may not need to include the inversion as it should probably
@@ -20,9 +29,13 @@ def expand_symmetry_related_planes(hkl, energies, spacegroup):
         reduced = tuple(x / np.gcd.reduce(x).max())
         if reduced not in unique or tiled_energies[unique[reduced]] > tiled_energies[i]:
             unique[reduced] = i
-        reduced_neg = tuple(-v for v in reduced)
-        if reduced_neg not in unique or tiled_energies[unique[reduced_neg]] > tiled_energies[i]:
-            unique[reduced_neg] = i
+
+        # reduced_neg = tuple(-v for v in reduced)
+        # if (
+        #     reduced_neg not in unique
+        #     or tiled_energies[unique[reduced_neg]] > tiled_energies[i]
+        # ):
+        #     unique[reduced_neg] = i
 
     unique_facets = []
     unique_energies = []
@@ -31,16 +44,23 @@ def expand_symmetry_related_planes(hkl, energies, spacegroup):
         unique_energies.append(tiled_energies[v])
     return np.array(unique_facets), np.array(unique_energies)
 
+
 def project_to_plane(points, plane_normal):
     projected_points = points - np.outer(np.dot(points, plane_normal), plane_normal)
-    
+    if projected_points.shape[0] < 2:
+        raise ValueError(
+            "Insufficient points for projection. Caused by projected points: {projected_points} from pts: {points}."
+        )
+
     a_vector = projected_points[1] - projected_points[0]
+
     b_vector = np.cross(plane_normal, a_vector)
-    
+
     u = np.dot(projected_points, a_vector)
     v = np.dot(projected_points, b_vector)
-    
+
     return np.column_stack((u, v))
+
 
 def winding_order_ccw(points):
     "return the indices to reorder the provided 2D points into CCW order"
@@ -48,15 +68,18 @@ def winding_order_ccw(points):
     directions = points[1:] - centroid
     directions /= np.linalg.norm(directions, axis=1)[:, np.newaxis]
     idxs = list(range(1, points.shape[0]))
-    return [0] + sorted(idxs, key=lambda x: np.arctan2(directions[x - 1, 1], directions[x - 1, 0]))
+    return [0] + sorted(
+        idxs, key=lambda x: np.arctan2(directions[x - 1, 1], directions[x - 1, 0])
+    )
 
 
 def prune_degenerate_points(points, threshold=1e-5):
-    dist_sq = np.sum((points[:, np.newaxis] - points)**2, axis=2)
+    dist_sq = np.sum((points[:, np.newaxis] - points) ** 2, axis=2)
     dist_sq[np.tril_indices_from(dist_sq)] = 1
     keep_mask = np.all(dist_sq >= threshold**2, axis=1)
     idxs = np.nonzero(keep_mask)[0]
     return points[keep_mask], idxs
+
 
 def ordered_facets(points, facets, facet_normals):
     result = []
@@ -70,6 +93,7 @@ def ordered_facets(points, facets, facet_normals):
         result.append([facet[idxs[x]] for x in ccw_order])
     return result
 
+
 def order_and_triangulate_polygons(points, facets, facet_normals):
     # probably the bottleneck
     ordered = ordered_facets(points, facets, facet_normals)
@@ -78,12 +102,14 @@ def order_and_triangulate_polygons(points, facets, facet_normals):
     facet_indices = []
     for i, facet in enumerate(ordered):
         N = len(facet)
-        if N == 0: continue
-        t = np.column_stack((np.repeat(facet[0], N-2), facet[1:N-1], facet[2:N]))
+        if N == 0:
+            continue
+        t = np.column_stack((np.repeat(facet[0], N - 2), facet[1 : N - 1], facet[2:N]))
         triangles.append(t)
         facet_indices += [i] * t.shape[0]
 
     return ordered, np.vstack(triangles), facet_indices
+
 
 def perpendicular_vector(p, q, r):
     "a unit vector perpendicular to the triangle p q r"
@@ -92,15 +118,21 @@ def perpendicular_vector(p, q, r):
     if dp > 0:
         return perp_vector / np.linalg.norm(perp_vector)
     else:
-        return - perp_vector / np.linalg.norm(perp_vector)
+        return -perp_vector / np.linalg.norm(perp_vector)
 
 
 class WulffSHT:
-    def __init__(self, facet_normals, facet_energies, labels=None,
-                 l_max=10, sht_object=None, scale=1):
+    def __init__(
+        self,
+        facet_normals,
+        facet_energies,
+        labels=None,
+        l_max=10,
+        sht_object=None,
+        scale=1,
+    ):
         from chmpy.shape.sht import SHT
 
-        
         self.facet_normals = facet_normals
         self.facet_energies = facet_energies
         self.labels = labels
@@ -114,8 +146,8 @@ class WulffSHT:
             sht = sht_object
 
         self.grid = np.array(sht.grid_cartesian)
-        dps = np.einsum('ijk,li->jkl', self.grid, self.facet_normals)
-        mask = (dps > 0)
+        dps = np.einsum("ijk,li->jkl", self.grid, self.facet_normals)
+        mask = dps > 0
         intersections = np.inf * np.ones_like(dps)
         intersections[mask] = 1.0 / dps[mask]
         intersections *= scale * self.facet_energies
@@ -127,6 +159,7 @@ class WulffSHT:
 
     def invariants(self, **kwargs):
         from chmpy.shape.shape_descriptors import expand_coeffs_to_full, make_invariants
+
         coeff4inv = expand_coeffs_to_full(self.l_max, self.coeffs)
         invariants = make_invariants(
             self.l_max, coeff4inv, kinds=kwargs.get("kinds", "NP")
@@ -135,6 +168,7 @@ class WulffSHT:
 
     def invariants_kazhdan(self, sht_object=None, **kwargs):
         from chmpy.shape.sht import SHT
+
         if sht_object is None:
             sht = SHT(self.l_max)
         else:
@@ -143,6 +177,7 @@ class WulffSHT:
 
     def power_spectrum(self, sht_object=None, **kwargs):
         from chmpy.shape.sht import SHT
+
         if sht_object is None:
             sht = SHT(self.l_max)
         else:
@@ -151,7 +186,9 @@ class WulffSHT:
 
     @classmethod
     def from_gmf_and_crystal(cls, gmf, crystal, **kwargs):
-        hkl, energies = expand_symmetry_related_planes(gmf.hkl, gmf.energies, crystal.space_group)
+        hkl, energies = expand_symmetry_related_planes(
+            gmf.hkl, gmf.energies, crystal.space_group
+        )
         facet_normals = hkl @ crystal.uc.reciprocal_lattice
         facet_normals /= np.linalg.norm(facet_normals, axis=1)[:, None]
         return cls(facet_normals, energies, labels=energies, **kwargs)
@@ -173,10 +210,11 @@ class WulffConstruction:
         self._extract_wulff_from_dual_mesh()
         self._fix_wulff_mesh()
 
-
     def _populate_duals(self):
         self.facet_vectors = self.facet_normals * self.facet_energies[:, np.newaxis]
-        self.facet_dual_vectors = self.facet_vectors / (self.facet_vectors **2).sum(axis=1)[:, np.newaxis]
+        self.facet_dual_vectors = safe_divide(
+            self.facet_vectors, (self.facet_vectors**2).sum(axis=1)[:, np.newaxis]
+        )
 
     def _construct_dual_space_hull(self):
         self.dual_hull = ConvexHull(self.facet_dual_vectors)
@@ -184,18 +222,17 @@ class WulffConstruction:
     def _extract_wulff_from_dual_mesh(self):
         # Get the simplices and reshape them for indexing
         simplices = self.dual_hull.simplices
-        tmp = self.facet_dual_vectors[simplices]
         a, b, c = np.rollaxis(self.facet_dual_vectors[simplices], 1)
 
         normals = np.cross(b - a, c - a)
 
         # Get the corresponding facet indices and normals
-        facet_indices = simplices[:, 0]  # Take first index from each simplex
+        facet_indices = simplices[:, 0]
 
         corresponding_facet_normals = self.facet_normals[facet_indices]
         corresponding_facet_energies = self.facet_energies[facet_indices]
-        inv_factors = np.einsum('ij,ij->i', normals, corresponding_facet_normals)
-        scaling_factors = corresponding_facet_energies / inv_factors
+        inv_factors = np.einsum("ij,ij->i", normals, corresponding_facet_normals)
+        scaling_factors = safe_divide(corresponding_facet_energies, inv_factors)
         vertices = normals * scaling_factors[:, np.newaxis]
 
         num_dual_vectors = len(self.facet_dual_vectors)
@@ -208,36 +245,41 @@ class WulffConstruction:
         self.wulff_vertices = vertices
         self.wulff_facets = facets
 
-
     def triangle_labels(self):
         return self.labels[self.wulff_triangle_indices]
 
     def _fix_wulff_mesh(self):
-        result = order_and_triangulate_polygons(self.wulff_vertices, self.wulff_facets, self.facet_normals)
+        result = order_and_triangulate_polygons(
+            self.wulff_vertices, self.wulff_facets, self.facet_normals
+        )
         self.wulff_facets = result[0]
         self.wulff_triangles = np.array(result[1])
         self.wulff_triangle_indices = np.array(result[2])
 
-
     def to_trimesh(self):
         from trimesh import Trimesh
+
         return Trimesh(self.wulff_vertices, self.wulff_triangles)
 
     def dual_trimesh(self):
         from trimesh import Trimesh
+
         return Trimesh(self.facet_dual_vectors, self.dual_hull.simplices)
 
     def sht(self, **kwargs):
-        return WulffSHT(self.facet_normals, self.facet_energies, labels=self.labels, **kwargs)
+        return WulffSHT(
+            self.facet_normals, self.facet_energies, labels=self.labels, **kwargs
+        )
 
     @classmethod
     def from_gmf_and_crystal(cls, gmf, crystal):
-        hkl, energies = expand_symmetry_related_planes(gmf.hkl, gmf.energies, crystal.space_group)
+        hkl, energies = expand_symmetry_related_planes(
+            gmf.hkl, gmf.energies, crystal.space_group
+        )
         facet_normals = hkl @ crystal.uc.reciprocal_lattice
         facet_normals /= np.linalg.norm(facet_normals, axis=1)[:, None]
 
         return cls(facet_normals, energies, labels=energies)
-
 
     def __repr__(self):
         return f"WulffConstruction<verts={len(self.wulff_vertices)}, facets={len(self.wulff_facets)}>"
